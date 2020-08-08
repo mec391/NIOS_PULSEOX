2b449c7ce26feec1f4197524b2807d0b444122eb
mec391 <56007839+mec391@users.noreply.github.com>
2020-08-07 21:57:10 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
+#define FIXED_POINT 16
 #include "altera_avalon_uart.h"
 #include "altera_avalon_uart_regs.h"
 # include <stdio.h>
 # include <stddef.h>
 #include <time.h>
+#include "kiss_fftr.h"
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+unsigned int receive(char prebuffer1[], char prebuffer2[]);
+void perform_comp(signed int buff16_1[], signed int buff16_2[], unsigned int *hr, unsigned int *spo2);
+void transmit(unsigned int hr, unsigned int spo2);
 
 int main()
 {
@@ -17,7 +27,10 @@ int main()
 	char prebuffer2[3];
 	unsigned long int buffer1[1024];
 	unsigned long int buffer2[1024];
+	signed int buff16_1[1024];
+	signed int buff16_2[1024];
 	unsigned int start_cnt = 0;
+	unsigned int numbytes;
 	while (start_cnt < 1024)
 	{
 
@@ -26,6 +39,8 @@ int main()
 		{
 			buffer1[start_cnt] = (unsigned long int){prebuffer1[0], prebuffer1[1], prebuffer1[2]};
 			buffer2[start_cnt] = (unsigned long int){prebuffer2[0], prebuffer2[1], prebuffer2[2]};
+			buff16_1[start_cnt] =(signed int) buffer1[start_cnt]>>6;
+			buff16_2[start_cnt] =(signed int) buffer2[start_cnt]>>6;
 			start_cnt = start_cnt + 1;
 		}
 
@@ -35,13 +50,15 @@ int main()
 		unsigned int hr;
 		unsigned int spo2;
 		unsigned int buffer_cnt = 0;
-		perform_comp(buffer1, buffer2, &hr, &spo2);
+		perform_comp(buff16_1, buff16_2, &hr, &spo2);
 		transmit(hr, spo2);
 		while (numbytes < 6) receive(prebuffer1, prebuffer2);
 		if (buffer_cnt == 1023)	buffer_cnt = 0;
 		else buffer_cnt = buffer_cnt + 1;
 		buffer1[buffer_cnt] = (unsigned long int){prebuffer1[0], prebuffer1[1], prebuffer1[2]};
 		buffer2[buffer_cnt] = (unsigned long int){prebuffer2[0], prebuffer2[1], prebuffer2[2]};
+		buff16_1[start_cnt] =(signed int) buffer1[start_cnt]>>6;
+		buff16_2[start_cnt] =(signed int) buffer2[start_cnt]>>6;
 }
 
 unsigned int receive(char prebuffer1[], char prebuffer2[])
@@ -51,11 +68,11 @@ unsigned int receive(char prebuffer1[], char prebuffer2[])
 	FILE* fp2;
 
 	fp1 = fopen("/dev/uart_1", "r+");
-	numbytes = fread(led_1, size_t(led_1), 3, fp);
+	numbytes = fread(prebuffer1, sizeof(prebuffer1)/sizeof(prebuffer1[0]), 3, fp1);
 	fclose(fp1);
 
 	fp2 = fopen("/dev/uart_2", "r+");
-	numbytes = numbytes + fread(led_2, size_t(led_2), 3, fp);
+	numbytes = numbytes + fread(prebuffer2, sizeof(prebuffer2)/sizeof(prebuffer2[0]), 3, fp2);
 	fclose(fp2);
 
 	return numbytes;
@@ -64,18 +81,66 @@ unsigned int receive(char prebuffer1[], char prebuffer2[])
 void transmit(unsigned int hr, unsigned int spo2)
 {
 
-
-
+//for now write to jtag uart instead of back to the device
+printf("HR = %d		     SPO2 = %d", hr, spo2);
 
 
 
 }
 
 
-void perform_comp(unsigned long int buffer1[], unsigned long int buffer2[])
+void perform_comp(signed int buff16_1[], signed int buff16_2[], unsigned int *hr, unsigned int *spo2)
 {
+	unsigned int nfft = 1024;
+	kiss_fftr_cfg cfg = kiss_fftr_alloc(nfft, 0, 0, 0);
+	kiss_fft_scalar cx_in[nfft];
+	for (int i = 0; i < nfft; i++) cx_in[i] = buff16_1[i];
+	kiss_fft_cpx cx_out[nfft/2+1];
 
 
 
+	kiss_fftr(cfg, cx_in, cx_out);
+	unsigned int DC_1 = sqrt(cx_out[0].r * cx_out[0].r + cx_out[0].i * cx_out[0].i);
+    unsigned int sort[512];
+    unsigned int AC_1 = 0;
+    unsigned int HR_1 = 0;
+    for (int i = 13; i < 512; i++) sort[i] = sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i);
+    for (int i = 13; i < 512; i++)
+		{
+			if (sort[i] > AC_1);
+			{
+				AC_1 = sort[i];
+				HR_1 = i;
+			}
+		}
+	free(cfg);
+//repeat for the second led
+		 nfft = 1024;
+		kiss_fftr_cfg cfg2 = kiss_fftr_alloc(nfft, 0, 0, 0);
+		kiss_fft_scalar cx_in2[nfft];
+		for (int i = 0; i < nfft; i++) cx_in[i] = buff16_2[i];
+		kiss_fft_cpx cx_out2[nfft/2+1];
+
+
+
+		kiss_fftr(cfg2, cx_in2, cx_out2);
+		unsigned int DC_2 = sqrt(cx_out2[0].r * cx_out2[0].r + cx_out2[0].i * cx_out2[0].i);
+
+	    unsigned int AC_2 = 0;
+	    unsigned int HR_2 = 0;
+	    for (int i = 13; i < 512; i++) sort[i] = sqrt(cx_out2[i].r * cx_out2[i].r + cx_out2[i].i * cx_out2[i].i);
+	    for (int i = 13; i < 512; i++)
+			{
+				if (sort[i] > AC_2);
+				{
+					AC_2 = sort[i];
+					HR_2 = i;
+				}
+			}
+		free(cfg2);
+//compute spo2
+unsigned int spo2_calc = (AC_1 / DC_1) / (AC_2 / DC_2) * 100;
+*hr = HR_1;
+*spo2 = spo2_calc;
 }
 
--- nios_data_buffer.v
+++ nios_data_buffer.v
@@ -260,7 +260,7 @@ wire td_new_data;
 
 assign HR_out = HR;   //otuput data to fio
 assign SPO2_out = SPO2; //output data to fifo
-assign final_comp_complete =  ///put the dv for comp done here
+//assign final_comp_complete =  ///put the dv for comp done here
 
 /*
 always@(posedge clk)
@@ -348,27 +348,290 @@ fft_buffer_led1_rhiddi_ftt fftbuff0(
 
 //pio_0 is input into nios
 //pio_1 is output to hardware
+reg [3:0] cnt = 0;
+reg [7:0] downsampler = 0;
+always@(posedge clk)
+begin
 
-always@(posedge clk) 
-begin 
-	if (
+	case (cnt)
+	4'd0:begin if (new_samples) cnt <= 1;
+		  else cnt <= cnt;end
+    4'd1:begin if(downsampler == 12)
+    	  begin
+    	  	downsampler <= 0;
+    	  	cnt <= 4'd2;
+    	  end
+    	  else
+    	  begin
+    	  	downsampler <= downsampler + 1;
+    	  	cnt <= 0;
+    	  end end
+    4'd2:begin uart_tx_data1 <= {2'b0, led_one[21:16]};
+    		   tx_dv1 <= 1;
+    		   cnt <= 3;end
+   	4'd3:begin	tx_dv1 <= 0;
+   				if(tx_done1 == 1) cnt <= 4;
+   				else cnt <= cnt;end
+   	4'd4:begin
+   				uart_tx_data1 <= led_one[15:8];
+   				tx_dv1 <= 1;
+   				cnt <= 5;end
+   	4'd5:begin 
+   				tx_dv1 <= 0;
+   				if(tx_done1 == 1) cnt <= 6;
+   				else cnt <= cnt;end
+   	4'd6:begin
+   				uart_tx_data1 <= led_one[7:0];
+   				tx_dv1 <= 1;
+   				cnt <= 7;end
+   	4'd7:begin
+   				tx_dv1 <= 0;
+   				uart_tx_data2 <= {2'b0, led_two[21:16]};
+   				tx_dv2 <= 1;
+   				cnt = 8;end
+   	4'd8:begin
+   				tx_dv2 <= 0;
+   				if(tx_done2 == 1) cnt <= 9;
+   				else cnt <= cnt;end
+    4'd9:begin
+    			uart_tx_data2 <= led_two[15:8];
+    			tx_dv2 <= 1;
+    			cnt = 10;end
+    4'd10:begin
+    			tx_dv2 <= 0;
+    			if(tx_done2 == 1)cnt <= 11;
+    			else cnt <= cnt;end
+    4'd11:begin
+    			uart_tx_data2 <= led_two[7:0];
+    			tx_dv2 <= 1;
+    			cnt = 12;end
+   	4'd12:begin
+   				tx_dv2 <= 0;
+   				cnt = 0;end
+	endcase
 end
+
+reg tx_dv1;
+reg uart_tx_data1;
+wire tx_busy1;
+wire tx1;
+wire tx_done1;
+reg tx_dv2;
+reg uart_tx_data2;
+wire tx_busy2;
+wire tx2;
+wire tx_done2;
+wire half_clk;
+
+divide_by_2 div5000(
+ .clk (clk),
+ .reset_n (reset_n),
+ .half_clk (half_clk)
+
+
+	);
+
+uart_tx1 utx1(
+.i_Clock (half_clk),
+.i_Tx_DV (tx_dv1),
+.i_Tx_Byte (uart_tx_data1),
+.o_Tx_Active (tx_busy1),
+.o_Tx_Serial (tx1),
+.o_Tx_Done (tx_done1),
+.reset_n (reset_n)
+  );
+
+uart_tx1 utx2(
+.i_Clock (half_clk),
+.i_Tx_DV (tx_dv2),
+.i_Tx_Byte (uart_tx_data2),
+.o_Tx_Active (tx_busy2),
+.o_Tx_Serial (tx2),
+.o_Tx_Done (tx_done2),
+.reset_n (reset_n)
+  );
 
 	soc_system u0 (
 		.clk_clk                          (clk),                          //                        clk.clk
-		.pio_0_external_connection_export (<connected-to-pio_0_external_connection_export>), //  pio_0_external_connection.export
-		.pio_1_external_connection_export (<connected-to-pio_1_external_connection_export>), //  pio_1_external_connection.export
+		.pio_0_external_connection_export (), //  pio_0_external_connection.export
+		.pio_1_external_connection_export (), //  pio_1_external_connection.export
 		.reset_reset_n                    (reset_n),                    //                      reset.reset_n
-		.uart_0_external_connection_rxd   (<connected-to-uart_0_external_connection_rxd>),   // uart_0_external_connection.rxd
-		.uart_0_external_connection_txd   (<connected-to-uart_0_external_connection_txd>),   //                           .txd
-		.uart_1_external_connection_rxd   (<connected-to-uart_1_external_connection_rxd>),   // uart_1_external_connection.rxd
-		.uart_1_external_connection_txd   (<connected-to-uart_1_external_connection_txd>),   //                           .txd
-		.uart_2_external_connection_rxd   (<connected-to-uart_2_external_connection_rxd>),   // uart_2_external_connection.rxd
-		.uart_2_external_connection_txd   (<connected-to-uart_2_external_connection_txd>)    //                           .txd
+		.uart_0_external_connection_rxd   (),   // uart_0_external_connection.rxd
+		.uart_0_external_connection_txd   (),   //                           .txd
+		.uart_1_external_connection_rxd   (tx1),   // uart_1_external_connection.rxd
+		.uart_1_external_connection_txd   (),   //                           .txd
+		.uart_2_external_connection_rxd   (tx2),   // uart_2_external_connection.rxd
+		.uart_2_external_connection_txd   ()    //                           .txd
 	);
 
 
 
 endmodule
+
+
+//////////////////////////////////////////////////////////////////////
+// File Downloaded from http://www.nandland.com
+//////////////////////////////////////////////////////////////////////
+// This file contains the UART Transmitter.  This transmitter is able
+// to transmit 8 bits of serial data, one start bit, one stop bit,
+// and no parity bit.  When transmit is complete o_Tx_done will be
+// driven high for one clock cycle.
+//
+// Set Parameter CLKS_PER_BIT as follows:
+// CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
+// Example: 10 MHz Clock, 115200 baud UART
+// (10000000)/(115200) = 87
+  
+//////////////////////////////////////////////////////////////////////
+// File Downloaded from http://www.nandland.com
+//////////////////////////////////////////////////////////////////////
+// This file contains the UART Transmitter.  This transmitter is able
+// to transmit 8 bits of serial data, one start bit, one stop bit,
+// and no parity bit.  When transmit is complete o_Tx_done will be
+// driven high for one clock cycle.
+//
+// Set Parameter CLKS_PER_BIT as follows:
+// CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
+// Example: 10 MHz Clock, 115200 baud UART
+// (10000000)/(115200) = 87
+  
+module uart_tx1 
+  #(parameter CLKS_PER_BIT = 217)
+  (
+   input       i_Clock,
+   input reset_n,
+   input       i_Tx_DV,
+   input [7:0] i_Tx_Byte, 
+   output      o_Tx_Active,
+   output reg  o_Tx_Serial,
+   output      o_Tx_Done
+   );
+  
+  parameter s_IDLE         = 3'b000;
+  parameter s_TX_START_BIT = 3'b001;
+  parameter s_TX_DATA_BITS = 3'b010;
+  parameter s_TX_STOP_BIT  = 3'b011;
+  parameter s_CLEANUP      = 3'b100;
+   
+  reg [2:0]    r_SM_Main     = 0;
+  reg [7:0]    r_Clock_Count = 0;
+  reg [2:0]    r_Bit_Index   = 0;
+  reg [7:0]    r_Tx_Data     = 0;
+  reg          r_Tx_Done     = 0;
+  reg          r_Tx_Active   = 0;
+     
+  always @(posedge i_Clock)
+    begin
+       if(~reset_n)
+       begin 
+       r_SM_Main <= s_IDLE;
+       end
+      case (r_SM_Main)
+        s_IDLE :
+          begin
+            o_Tx_Serial   <= 1'b1;         // Drive Line High for Idle
+            r_Tx_Done     <= 1'b0;
+            r_Clock_Count <= 0;
+            r_Bit_Index   <= 0;
+             
+            if (i_Tx_DV == 1'b1)
+              begin
+                r_Tx_Active <= 1'b1;
+                r_Tx_Data   <= i_Tx_Byte;
+                r_SM_Main   <= s_TX_START_BIT;
+              end
+            else
+              r_SM_Main <= s_IDLE;
+          end // case: s_IDLE
+         
+         
+        // Send out Start Bit. Start bit = 0
+        s_TX_START_BIT :
+          begin
+            o_Tx_Serial <= 1'b0;
+             
+            // Wait CLKS_PER_BIT-1 clock cycles for start bit to finish
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_START_BIT;
+              end
+            else
+              begin
+                r_Clock_Count <= 0;
+                r_SM_Main     <= s_TX_DATA_BITS;
+              end
+          end // case: s_TX_START_BIT
+         
+         
+        // Wait CLKS_PER_BIT-1 clock cycles for data bits to finish         
+        s_TX_DATA_BITS :
+          begin
+            o_Tx_Serial <= r_Tx_Data[r_Bit_Index];
+             
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_DATA_BITS;
+              end
+            else
+              begin
+                r_Clock_Count <= 0;
+                 
+                // Check if we have sent out all bits
+                if (r_Bit_Index < 7)
+                  begin
+                    r_Bit_Index <= r_Bit_Index + 1;
+                    r_SM_Main   <= s_TX_DATA_BITS;
+                  end
+                else
+                  begin
+                    r_Bit_Index <= 0;
+                    r_SM_Main   <= s_TX_STOP_BIT;
+                  end
+              end
+          end // case: s_TX_DATA_BITS
+         
+         
+        // Send out Stop bit.  Stop bit = 1
+        s_TX_STOP_BIT :
+          begin
+            o_Tx_Serial <= 1'b1;
+             
+            // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_STOP_BIT;
+              end
+            else
+              begin
+                r_Tx_Done     <= 1'b1;
+                r_Clock_Count <= 0;
+                r_SM_Main     <= s_CLEANUP;
+                r_Tx_Active   <= 1'b0;
+              end
+          end // case: s_Tx_STOP_BIT
+         
+         
+        // Stay here 1 clock
+        s_CLEANUP :
+          begin
+            r_Tx_Done <= 1'b1;
+            r_SM_Main <= s_IDLE;
+          end
+         
+         
+        default :
+          r_SM_Main <= s_IDLE;
+         
+      endcase
+    end
+ 
+  assign o_Tx_Active = r_Tx_Active;
+  assign o_Tx_Done   = r_Tx_Done;
+   
+endmodule
+
 
 
